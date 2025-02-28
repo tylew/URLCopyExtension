2b693352de8222a3481f330953f30e81844a6e63
Tyler L <69596889+tylew@users.noreply.github.com>
2025-02-28 14:51:04 -0800

    Initial Commit

---
+++ URLCopyExtension/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  URLCopyExtension
+//
+//  Created by fsl-dev on 2/28/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ URLCopyExtensionTests/URLCopyExtensionTests.swift
@@ -0,0 +1,17 @@
+//
+//  URLCopyExtensionTests.swift
+//  URLCopyExtensionTests
+//
+//  Created by fsl-dev on 2/28/25.
+//
+
+import Testing
+@testable import URLCopyExtension
+
+struct URLCopyExtensionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
